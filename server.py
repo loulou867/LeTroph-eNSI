e3a738b45b81a228cbc6ad8f1880afb4ca5a3a1d
loulou867 <88657702+loulou867@users.noreply.github.com>
2024-03-20 22:53:53 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,82 +1,62 @@
-#we need a server sys and a client sys 
-
-#import required stuffs which wil be sockets and threadinng
-#connection
-#starting the server
-#adding lists -- which is client (leaving that empty so that they can add their name after joining) --- mr.robot style  ;)
-#methods required for the chat  -- Ill combine all methods with a main method
-##definig main receive functions -- Broadcast, handler, receiving and listining function
-
-
 import socket
 import threading
 
+# Données de connexion
+hôte = '127.0.0.1'  # C'est l'adresse IP de notre ordinateur.
+port = 12345        # C'est le port que nous utilisons pour la connexion.
 
-# Connection Data  
-#Port is confidential thing
-host = '0.0.0.0'                                                 #IP of your host
-port = 12345                                                       #Dont take reserved ports
+# Commençons par lancer le serveur.
+serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Création d'un tunnel TCP en IPV4
+serveur.bind((hôte, port))                                  # Relion-le à notre adresse IP et notre port.
+serveur.listen()                                            # Mettons-le en mode écoute.
 
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         #Internet socket
-server.bind((host, port))                                          #Server is binded to local host and a port  
-server.listen()                                                    #Server is set to listining mode
-
-# Lists For Clients and Their Nicknames
+# Créons des listes pour stocker les clients et leurs pseudonymes.
 clients = [] 
-nicknames = []
+pseudonymes = []
 
-# Sending Messages To All Connected Clients
-def broadcast(message): 
+# Diffusion des messages du client à tous les autres clients connectés.
+def diffusion(message): 
     for client in clients:
-        client.send(message)                                        #Getting all the clients and sending msg that new one joined to the admin
+        client.send(message)  # Envoyons le message à chaque client.
 
-# Handling Messages From Clients
-def handle(client):
+# Gérons les messages reçus des clients.
+def gestion(client):
     while True:
         try:
-            # Broadcasting Messages
-            message = client.recv(1024)                      
-            broadcast(message)                                      #After receiving we are broadcasting to other
+            # Listening des messages.
+            message = client.recv(1024)  # .recv(1024 bytes) = recoit le message                    
+            diffusion(message)  # Après avoir reçu un message, renvoyer-le à tout le monde.
         except:
-            # Removing And Closing Clients
+            # Si client se déconnecte, retire le de la liste.
             index = clients.index(client)        
             clients.remove(client)       
-            client.close()
-            nickname = nicknames[index]                             #When we remove client we'l also remove nicknamesw with help of index
-            broadcast('{} left!'.format(nickname).encode('ascii'))  
-            nicknames.remove(nickname)
+            client.close() # fermeture du client
+            pseudonyme = pseudonymes[index]  # Retirons également son pseudonyme.
+            diffusion('{} a quitté!'.format(pseudonyme).encode('ascii'))  
+            pseudonymes.remove(pseudonyme)
             break        
 
-#These Functions will run later on in a thread so evrything will be running
-#Main method receive to combine all methods
-
-
-# Receiving / Listening Function
-def receive():
+# Fonction pour recevoir les connexions des clients.
+def recevoir():
     while True:
-        # Accept Connection
-        client, address = server.accept()                          #In server IP wil login so different IP will login with same port on the server IP
-        print("Connected with {}".format(str(address)))            #Waiting for the connection and accepting the connections
+        # Accepter les connexions entrantes.
+        client, adresse = serveur.accept()  
+        print("Connecté avec {}".format(str(adresse)))  
 
-        #Getting nickname by asking --- code word sent the client and the client side accepts the encoded code which is NICK here and send backs the message that he can join
-        #Request And Store Nickname
-
-        client.send('NICK'.encode('ascii'))                        #Sending in encoded form 
-        nickname = client.recv(1024).decode('ascii')                
-        nicknames.append(nickname)
+        # demande au client son pseudonyme.
+        client.send('NICK'.encode('ascii'))  
+        pseudonyme = client.recv(1024).decode('ascii')  
+        pseudonymes.append(pseudonyme)
         clients.append(client)
 
-        # Print And Broadcast Nickname  
-        #pritning
-        print("Name is {}".format(nickname))
-        #broadcasting
-        broadcast("{} joined!".format(nickname).encode('ascii'))   #So that every client will know new connection is joined
-        client.send('Connected to server!'.encode('ascii'))        #Connected and can starting
+        # Affichons le nickname du nouveau client.
+        print("Le pseudonyme est {}".format(pseudonyme))
+        diffusion("{} a rejoint!".format(pseudonyme).encode('ascii'))  
+        client.send('Connecté au serveur!'.encode('ascii'))  
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))   #This is because multiple client send multiple messages and so to avoid crashing we are handling with handler
-        thread.start()                                             #To work with threads you need start method or run method
+        # Demarre un thread pour gérer les messages du client.
+        thread = threading.Thread(target=gestion, args=(client,))  
+        thread.start()  
 
-print("Server is started................,:) ")
-receive()
+print("Le serveur est prêt à recevoir des messages !")
+recevoir()  # Allons-y !
