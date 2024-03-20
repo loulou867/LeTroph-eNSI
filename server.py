4760a7e88ef144dd627e2af6e90838acdf5522b3
loulou867 <88657702+loulou867@users.noreply.github.com>
2024-03-20 20:15:43 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,103 +1,56 @@
 import socket
-import threading
+from cryptography.fernet import Fernet
+from base64 import urlsafe_b64encode
 
-# Adresse IP et ports du serveur
-IP_SERVER = "0.0.0.0"  # Laissez cette chaîne vide pour lier le serveur à toutes les interfaces disponibles
-PORT_HTTPS = 443
-PORT_OTHER = 6969
+from util import generate_key, format_key
 
-# Création du socket serveur pour HTTPS
-server_https = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_https.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_https.bind((IP_SERVER, PORT_HTTPS))
-
-# Création du socket serveur pour l'autre port
-server_other = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_other.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_other.bind((IP_SERVER, PORT_OTHER))
-
-# Nombre maximal de connexions en attente autorisées (backlog)
-server_https.listen(5)
-server_other.listen(5)
-
-# Affichage de l'adresse IP et des ports du serveur
-if IP_SERVER:
-    print(f"[*] Server running, listening on {IP_SERVER}")
-else:
-    IP_SERVER = socket.gethostbyname(socket.gethostname())
-    print(f"[*] Server running, listening on {IP_SERVER}")
-
-print(f"[*] Server listening on HTTPS port {PORT_HTTPS}")
-print(f"[*] Server listening on other port {PORT_OTHER}")
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 6969
 
 
-# Fonction pour gérer les messages entrants du client
-def handle_received_message(client_socket: socket.socket):
-    while True:
-        try:
-            # Recevoir le message du client
-            message = client_socket.recv(1024).decode('utf-8')
-            if message:
-                print(f"[Client]: {message}")
-                # Diffuser le message à tous les autres clients
-                broadcast_message(message, client_socket)
-            else:
-                # Fermer la connexion si le message est vide
-                client_socket.close()
-                break
-        except Exception as e:
-            print(f'[!] Error handling message from client: {e}')
-            client_socket.close()
-            break
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((SERVER_HOST, SERVER_PORT))
+    sock.listen(5)
 
+    print(f'Running in {SERVER_HOST}:{SERVER_PORT}\n')
+    print('- Wait connection...')
 
-# Fonction pour diffuser un message à tous les clients sauf l'expéditeur
-def broadcast_message(message: str, sender_socket: socket.socket):
-    for client in clients:
-        if client != sender_socket:
-            try:
-                client.send(message.encode('utf-8'))
-            except Exception as e:
-                print(f'[!] Error broadcasting message to client: {e}')
-                client.close()
-                clients.remove(client)
+    client, __ = sock.accept()
+    print('- Connected, performing key exchange...')
 
+    # keys received and generated
+    common = int(client.recv(1024).decode())
+    secret = generate_key()
+    public = common + secret
 
-# Liste pour stocker les connexions des clients
-clients = []
+    # sending "public" key and waiting for peer's public key
+    client.send(str(public).encode())
+    public_peer = int(client.recv(1024).decode())
+    message_secret = public_peer + secret
 
-# Fonction pour gérer les connexions des clients
-def handle_client_connection(server_socket: socket.socket):
-    while True:
-        try:
-            # Accepter une nouvelle connexion du client
-            client_socket, client_address = server_socket.accept()
-            print(f"[*] Accepted connection from: {client_address[0]}:{client_address[1]}")
-            # Ajouter le client à la liste des clients connectés
-            clients.append(client_socket)
-            # Démarrer un thread pour gérer les messages du client
-            threading.Thread(target=handle_received_message, args=(client_socket,)).start()
-        except Exception as e:
-            print(f'[!] Error accepting client connection: {e}')
-            break
+    print(f'- Encrypt key is: {format_key(message_secret)}\n')
 
+    key_bytes = message_secret.to_bytes(32, byteorder='little')
+    fernet = Fernet(urlsafe_b64encode(key_bytes))
 
-# Fonction pour démarrer le serveur
-def start_server(server_socket: socket.socket):
+    print('=' * 20)
+
     try:
-        while True:
-            # Gérer les connexions des clients
-            handle_client_connection(server_socket)
+        while True:            
+            # wait response
+            client_msg = client.recv(1024)
+            client_msg_decrypted = fernet.decrypt(client_msg)
+            print(f'client> {client_msg_decrypted.decode()}')
+
+            message = input('message> ').strip()
+            encrypted_msg = fernet.encrypt(message.encode())
+            client.send(encrypted_msg)
+            print('...')
     except KeyboardInterrupt:
-        print("[*] Server stopped.")
-    finally:
-        # Fermer tous les sockets clients restants
-        for client in clients:
-            client.close()
-        # Fermer le socket serveur
-        server_socket.close()
+        print('Bye.')
+        sock.close()
 
 
-# Démarrer le serveur dans un thread séparé pour chaque port
-threading.Thread(target=start_server, args=(server_https,)).start()
-threading.Thread(target=start_server, args=(server_other,)).start()
+main()
